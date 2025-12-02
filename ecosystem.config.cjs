68e847572549bc45a01775ca366489a2965f695c
salmeida1993 <stewartalmeida@yahoo.com>
2025-12-02 16:55:38 -0500

    Adjusting config.cjs to allow for ENV variables from Render.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,19 +3,10 @@ module.exports = {
     {
       name: "auth",
       script: "./auth-server/server.js",
-      env: {
-        AUTH_PORT: 4000,
-        NODE_ENV: "production"
-      }
     },
     {
       name: "backend",
       script: "./backend.js",
-      env: {
-        PORT: process.env.PORT || 3000,
-        AUTH_SERVICE_URL: "http://localhost:4000",
-        NODE_ENV: "production"
-      }
     }
   ]
 };
